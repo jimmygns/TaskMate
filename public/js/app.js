a7d57cfa2fabfc4cbaa6f1df05a49b4d2ca2cae6
jimmy <jimmygns@me.com>
2015-04-23 17:21:56 -0700

    Dynamic list

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
+
 function ListController($scope){
 	
-	Parse.initialize("bh9Iza9Ir2cWNXStutapOSqVT1YCG86nsFBlL5d7", "0Kmq6ErvBxzF6NtYOCobQD7F3qFuC5V2E1NnR2Dl");
+	Parse.initialize("ZLDPnL5t8AzKUzPF2OJfLcP7GGS5584iJSMGXkRS", "3VWB7w2Gri9KbRTmzKp8Lr6hInyAYaiSVjig87uB");
 	var List = Parse.Object.extend("List");
 	var query = new Parse.Query(List);
+	var array = [];
 
 	query.find({
 		success: function(results) {
@@ -10,15 +12,71 @@ function ListController($scope){
     // Do something with the returned Parse.Object values
     for (var i = 0; i < results.length; i++) { 
     	var object = results[i];
+    	var json='{"id":' + object.id +',' + '"name":' +object.get('Name') + '}';
+    	array.push(json);
+    	alert(object.id + ' - ' + object.get('Name'));
+    }
+    $scope.toDoLists=[];
+    for(var i = 0; i < array.length;i++){
+		$scope.toDoLists.push({name: array[i]});
+    }
+	$scope.$digest();
+},
+error: function(error) {
+	alert("alert");
+	alert("Error: " + error.code + " " + error.message);
+}
+
+
+});
+
+    
+
+	
+}
+/*
+(function(){
+
+	var gem =[
+      
+    {name: 'Zeus', price: 2.99},
+    {name: 'ruby',price:3.00}
+    ];
+	Parse.initialize("ZLDPnL5t8AzKUzPF2OJfLcP7GGS5584iJSMGXkRS", "3VWB7w2Gri9KbRTmzKp8Lr6hInyAYaiSVjig87uB");
+	var List = Parse.Object.extend("List");
+	var query = new Parse.Query(List);
+	var array = [];
+
+	query.find({
+		success: function(results) {
+			alert("Successfully retrieved " + results.length + " lists.");
+    // Do something with the returned Parse.Object values
+    for (var i = 0; i < results.length; i++) { 
+    	var object = results[i];
+    	var json='{"id":' + object.id +',' + '"name":' +object.get('Name') + '}';
+    	array.push(json);
     	alert(object.id + ' - ' + object.get('Name'));
     }
 },
 error: function(error) {
+	alert("alert");
 	alert("Error: " + error.code + " " + error.message);
 }
+
+
 });
 
-	$scope.toDoLists=query;
-}
 
 
+
+	
+	var app=angular.module("List",[]);
+	app.controller('ListController',function(){
+		this.products=gem;
+
+	});
+
+
+})();
+
+*/

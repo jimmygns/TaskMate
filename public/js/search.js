eddb51fd50ba0a334b3dc9e05cc08087d3cc5c64
jimmy <jimmygns@me.com>
2015-04-25 18:52:35 -0700

    search

---
+++ public/js/search.js
@@ -0,0 +1,36 @@
+function ListController($scope){
+
+
+Parse.initialize("ZLDPnL5t8AzKUzPF2OJfLcP7GGS5584iJSMGXkRS", "3VWB7w2Gri9KbRTmzKp8Lr6hInyAYaiSVjig87uB");
+
+var List = Parse.Object.extend("List");
+var query = new Parse.Query(List);
+var name = prompt("Enter the name: ");
+
+query.equalTo("Name",name);
+
+var array=[];
+
+query.find({
+  success: function(results) {
+    alert("Successfully retrieved " + results.length + " scores.");
+    // Do something with the returned Parse.Object values
+    for (var i = 0; i < results.length; i++) { 
+      var object = results[i];
+      array.push(object);
+      alert(object.id + ' - ' + object.get('Name'));
+    }
+
+    $scope.results=[];
+    for(var i = 0; i < array.length;i++){
+		$scope.results.push({name: array[object.get('Name')]});
+    }
+	$scope.$digest();
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
+
+
+}

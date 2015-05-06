9f6049ddde49271ee954dc6f95b1de46b0e905f9
Yue Zhao <jzhao156@gmail.com>
2015-05-06 00:12:10 -0700

    Created clouddemo

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,21 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("test", function(request, response) {
+    var list = Parse.Object.extend("List");
+    var query = new Parse.Query(list);
+
+
+
+    query.find({
+        success: function(results) {
+            alert("Successfully retrieved " + results.length + " scores.");
+            // Do something with the returned Parse.Object values
+            response.success("Found " + results.length + " lists");
+        },
+        error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+});
---
+++ public/js/clouddemo.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yuezhao on 5/5/15.
+ */
+Parse.initialize("eVEt0plCyNLg5DkNtgBidbruVFhqUBnsMGiiXp63", "KPiNXDn9LMX17tLlMmSbI4NvTKgWPk36qBLMTqco");
+Parse.Cloud.run('test', {}, {
+    success: function(result) {
+// result is 'Hello world!'
+        alert(result);
+    },
+    error: function(error) {
+        alert("Error1");
+    }
+});

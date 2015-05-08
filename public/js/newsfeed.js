3bebda56c0f5365e8be85baf49ffaaae33fb3490
Kexun Xu <k2xu@ucsd.edu>
2015-05-08 13:32:49 -0700

    modified newsfeed html/js files

---
+++ public/js/newsfeed.js
@@ -0,0 +1,6 @@
+Parse.initialize("u1Ja8cb4WbMF4LfhXc4DDxVumfE8mCuCZRTFOHSl", 
+	             "2YToN08vDrAA31y4031ihdFqdLxTmpJcBoL4TlDB");
+
+var currentEmail = Parse.User.current().email();
+alert(currentEmail);
+

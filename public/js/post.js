344529d60c3952889f44c221334be5accda1eec4
Qiuxin Sheng <Aki@Akis-MacBook-Pro.local>
2015-05-09 21:04:43 -0700

    display the message

--- public/js/post.js
+++ public/js/post.js
@@ -6,21 +6,20 @@ function NewsfeedController($scope){
 	var query = new Parse.Query(Newsfeed);
 	var id = "YqBoUQyWYs";
     query.equalTo("objectId",id);
+    var postsToDisplay = [];
+
 	query.find({
 		success: function(results) {
-			alert("Successfully retrieved " + results.length + " lists.");
-    // Do something with the returned Parse.Object values
-    for (var i = 0; i < results.length; i++) { 
+    for (var i = 0; i < results.length; i++) {
     	var object = results[i];
-    	//var json='{"id":' + object.id +',' + '"name":' +object.get('Name') + '}';
-    	//array.push(json);
-    	//alert(object.id + ' - ' + object.get('Name'));
+    	postsToDisplay.push(object.get('message'));
     }
-    $scope.toDoLists=[];
-    for(var i = 0; i < array.length;i++){
-		$scope.toDoLists.push({name: array[i]});
+    
+    $scope.posts = [];
+    for(var i=0; i<postsToDisplay.length; i++){
+    	$scope.posts.push({message: postsToDisplay[i]});
     }
-	$scope.$digest();
+    $scope.$digest();
 },
 error: function(error) {
 	alert("alert");

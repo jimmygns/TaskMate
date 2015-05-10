cc924ef1c11693791ed71f93ae2c4d53f55eec3d
Kexun Xu <k2xu@ucsd.edu>
2015-05-09 19:13:47 -0700

    passed string for search and profile

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -1,6 +1,17 @@
 Parse.initialize("QjXPXme3VpSyKdvZKMDRXY5TmdOP7L6fxvysfdXq",
  "FB03u9unUQsaHY6PDpt1rxVIlzBfIPkubtssIKSz");
 
-var currentEmail = Parse.User.current().email();
-alert(currentEmail);
+function redirect(){
+	var text = document.getElementById("searchInput").value;
+	//alert(text);
+	window.location.href = "../search.html?" + text;
+	//alert("../search.html?" + text);
+};
+
+function goToProfile(){
+	var text = Parse.User.current().id;
+	alert(text);
+	window.location.href = "../profile.html?" + text;
+};
+
 

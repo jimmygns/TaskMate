c674c7777ec865d3a2f96712492c85b357e55b43
Zhixing Meng <z3meng@ucsd.edu>
2015-05-15 12:33:13 -0700

    edited newsfeed files

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -24,4 +24,24 @@ function logOut(){
     window.location.href = "../index.html";
 };
 
+var newsfeedApp = angular.module("newsfeedApp", []);
 
+newsfeedApp.controller("newsfeedCtrl", ["$scope", function newsfeedCtrl($scope){
+    Parse.initialize("eVEt0plCyNLg5DkNtgBidbruVFhqUBnsMGiiXp63", "KPiNXDn9LMX17tLlMmSbI4NvTKgWPk36qBLMTqco");
+
+	var query = new Parse.Query("Newsfeed");
+	query.find({
+  	success: function(newsfeeds) {
+    	// newsfeeds now contains an array of newsfeed object retrieved from Parse
+    	alert("Successfully retrieved " + results.length + " Newsfeed objects.");
+    	for (var i = 0; i < results.length; i++){
+    		var newsfeed = newsfeeds[i];
+    	}
+  	},
+
+  	error: function(error) {
+    	alert("Error: " + error.code + " " + error.message);
+  	}
+	});
+
+}]);

8cf80a26d7670126948dd2eac1468f882dd22475
Zhixing Meng <z3meng@ucsd.edu>
2015-05-21 13:53:22 -0700

    finished the display of newsfeed

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -17,10 +17,7 @@ function goToProfile(){
 };
 
 function logOut(){
-	Parse.User.logOut();
-	//if(Parse.User.current() == null){
-	  //alert("logged out");
-	//}
+	Parse.User.logOut(); 
     window.location.href = "../index.html";
 };
 
@@ -30,17 +27,87 @@ newsfeedApp.controller("newsfeedCtrl", ["$scope", function newsfeedCtrl($scope){
     Parse.initialize("eVEt0plCyNLg5DkNtgBidbruVFhqUBnsMGiiXp63", "KPiNXDn9LMX17tLlMmSbI4NvTKgWPk36qBLMTqco");
 
 	var query = new Parse.Query("Newsfeed");
-	query.find({
+
+
+	query.find({ 
   	success: function(newsfeeds) {
     	// newsfeeds now contains an array of newsfeed object retrieved from Parse
-    	alert("Successfully retrieved " + results.length + " Newsfeed objects.");
-    	for (var i = 0; i < results.length; i++){
+    	alert("Successfully retrieved " + newsfeeds.length + " Newsfeed objects.");
+
+
+      $scope.posts = []; //array for Newsfeed
+
+
+    	for (var i = 0; i < newsfeeds.length; i++){
     		var newsfeed = newsfeeds[i];
+
+        
+/*      $scope.user_objectId = newsfeed.get('owner');
+    		var userQuery = new Parse.Query("User");
+    		userQuery.get($scope.user_objectId, {
+    			success: function(user) {
+    				//The object was retrieved successfully.
+            $scope.posts1.push(user.get('username'));
+    				//$scope.posts1[i].username = user.get('username');
+            $scope.$digest();
+    			},
+    			error: function(object, error){
+    				//The object was not retrieved successfully
+    				alert("Error: " + error.code + " " + error.message);
+    			} 
+    		});
+  				    				
+
+        
+        $scope.goal_objectId = newsfeed.get('goal');
+   			var goalQuery = new Parse.Query("Goal");
+    		goalQuery.get($scope.goal_objectId, {
+  				success: function(goal) {
+    				// The object was retrieved successfully.
+            $scope.posts2[i] = {};
+    				$scope.posts2[i].goal_name = goal.get('name');
+    				$scope.posts2[i].goal_dueDate = goal.get('dueDate');
+            $scope.$digest();
+  				},
+  				error: function(object, error) {
+    				// The object was not retrieved successfully.
+    				alert("Error: " + error.code + " " + error.message);
+  				}
+			  });
+
+        //no need to do that
+    		$scope.list_objectId = newsfeed.get('list');
+    		var listQuery = new Parse.Query("List");
+    		listQuery.get($scope.list_objectId, {
+  				success: function(list) {
+    				// The object was retrieved successfully.
+            $scope.posts3[i] = {};
+    				$scope.posts3[i].list_name = list.get('name');
+            $scope.$digest();
+  				},
+  				error: function(object, error) {
+    				// The object was not retrieved successfully.
+    				alert("Error: " + error.code + " " + error.message);
+  				}
+			  });    */
+
+        $scope.posts[i] = {};
+    		$scope.posts[i].message = newsfeed.get('message');
+    		$scope.posts[i].numLikes = newsfeed.get('numLikes');
+    		$scope.posts[i].numComments = newsfeed.get('numComments');
+        $scope.$digest();
+
+
+
+
     	}
+
+
+
   	},
 
   	error: function(error) {
-    	alert("Error: " + error.code + " " + error.message);
+    	//alert("Error: " + error.code + " " + error.message);
   	}
 	});
 

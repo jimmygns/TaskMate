ce299a87599c0a5af1109fdad6d20ef15ab05e50
Amelia Vu <adv002@ucsd.edu>
2015-06-01 14:27:35 -0700

    New navigation bar for newsfeed.

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -29,11 +29,41 @@ function goToNotification(){
 };
 
 
-
 var newsfeedApp = angular.module("newsfeedApp", []);
 
 newsfeedApp.controller("newsfeedCtrl", ["$scope", function newsfeedCtrl($scope){
     Parse.initialize("eVEt0plCyNLg5DkNtgBidbruVFhqUBnsMGiiXp63", "KPiNXDn9LMX17tLlMmSbI4NvTKgWPk36qBLMTqco");
+
+  $scope.numberOfNotification = Parse.User.current().get('numNotif');
+
+  $scope.profilePictureURL = "img/glyphicons-4-user.png";
+
+  picture = Parse.User.current().get("profilePicture");
+  if (picture != undefined) {
+    $scope.profilePictureURL = picture.url();
+  }
+
+  $scope.goHome = function() {
+      window.location.href = "./newsfeed.html";
+  }
+
+  $scope.goNotification = function() {
+    window.location.href = "./notifications.html";
+  }
+
+  $scope.search = function(){
+    window.location.href = "./search.html?" + $scope.searchInput;
+  };
+
+  $scope.goProfile = function(){
+    window.location.href = "./profile.html?" + Parse.User.current().id;
+  };
+
+  $scope.logOut = function(){
+    Parse.User.logOut();
+      window.location.href = "./index.html";
+  };
+
 
 
   $scope.goToPost = function(index){
